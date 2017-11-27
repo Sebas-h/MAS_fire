f0df091979a99865294cbd73b0dc8be319e8b11e
MaxVonKnobloch <max.vknobloch@gmail.com>
2017-11-27 21:20:36 +0100

    Changed complete task counter
    Task is complete as soon as firefighter sees that there is no more fire in the task gridpoint

--- fireFighters_MAS/Firefighter.java
+++ fireFighters_MAS/Firefighter.java
@@ -113,10 +113,7 @@ public class Firefighter {
 			return;
 		} // Safety
 		GridPoint myPos = grid.getLocation(this);
-		if (knowledge.getCurrentTask()!=null&&myPos.equals(knowledge.getCurrentTask())) {
-			((IGlobalCounter) context.getObjects(TaskCompleteCounter.class).get(0)).incrementCounter();
-			knowledge.setCurrentTask(null);
-		}
+		
 		// Info acquisition part (takes no time)
 		checkEnvironment(sightRange);
 		// increases a score for each known fire by 1
@@ -549,6 +546,10 @@ public class Firefighter {
 		if (hasFire) {
 			knowledge.addFire(pos);
 		} else {
+			if (knowledge.getCurrentTask()!=null&&pos.equals(knowledge.getCurrentTask())) {
+					((IGlobalCounter) context.getObjects(TaskCompleteCounter.class).get(0)).incrementCounter();
+					knowledge.setCurrentTask(null);
+			}
 			knowledge.removeFire(pos);
 		}
 
